51dfe90e1aabe2b23a38296a70ee93370ae9af7c
wittyneko <wittytutu@gmail.com>
2019-05-15 01:54:23 +0800

    添加子模块名字解析

--- libs/github/wittyneko/ktx-base/master/base.groovy
+++ libs/github/wittyneko/ktx-base/master/base.groovy
@@ -2,13 +2,17 @@ def repoInfo = 'github/wittyneko/ktx-base/master/base.groovy'.gitRepoInfo(null, 
 def repoZip = repoInfo.gitRepoZip()
 
 def module_tag = repoInfo.tags
-def module_name = "${repoInfo.module.name}.base"
-def module_repo = new File(repoInfo.module.repo, 'base')
+
+def module_sub = repoInfo.filename.with { substring(0, lastIndexOf('.')) }
+def module_name = "${repoInfo.module.name}.$module_sub"
+def module_repo = new File(repoInfo.module.repo, module_sub)
 def module_cache_dir = new File(repo_caches, module_name.replace('.', '/'))
 def module_cache_file = new File(module_cache_dir, "${module_tag}.zip")
 
 //println "$module_name info: " + repoInfo
 //println "$module_name zip: " + repoZip
+//println "$module_name zip: " + module_tag
+//println "$module_name sub: " + module_sub
 //println "$module_name repo: " + module_repo
 //println "$module_name cache: " + module_cache_dir
 
@@ -33,6 +37,6 @@ new File(module_cache_dir, module_tag).with {
 module_repo.with {
     if (!exists()) {
         parentFile?.mkdirs()
-        new File(module_cache_dir, "$module_tag/ktx-base-$module_tag/base").copyTo(it)
+        new File(module_cache_dir, "$module_tag/ktx-base-$module_tag/$module_sub").copyTo(it)
     }
 }
